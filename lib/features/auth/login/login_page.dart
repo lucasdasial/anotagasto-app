2301911e300970fa028e74c0f825cadde57e0927
Lucas Alves <as.lucasalves@gmail.com>
2026-03-02 11:50:40 -0400

    implement base example of viewModel

--- lib/features/auth/login/login_page.dart
+++ lib/features/auth/login/login_page.dart
@@ -1,4 +1,20 @@
+import 'package:anotagasto/core/ui_state.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class LoginViewModel extends ChangeNotifier {
+  UiState _state = Initial();
+  UiState get state => _state;
+
+  Future<void> login() async {
+    _state = Loading();
+    notifyListeners();
+
+    await Future.delayed(Duration(seconds: 4));
+    _state = Success("xpto_token");
+    notifyListeners();
+  }
+}
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,7 +23,27 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("AnotaGasto")),
-      body: Center(child: Text("Login em breve")),
+      body: ChangeNotifierProvider<LoginViewModel>(
+        create: (_) => LoginViewModel(),
+        child: Consumer<LoginViewModel>(
+          builder: (context, viewModel, child) {
+            if (viewModel.state is Loading) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (viewModel.state is Success) {
+              return Center(child: Text("Logado!"));
+            }
+
+            return Center(
+              child: FilledButton(
+                onPressed: viewModel.login,
+                child: Text("Entrar"),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
